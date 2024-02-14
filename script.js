d559fa5f797417bfb2d46a25a0b57f70e3393a63
MiriamEvardi <miriamevardi97@gmail.com>
2024-02-14 19:29:50 +0100

    add new images

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 const images = [
     {
-        image: 'img/01.webp',
+        image: 'wallhaven-7396ky.jpg',
         title: "Marvel's Spiderman Miles Morale",
         text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.'
     },
 
     {
-        image: 'img/02.webp',
+        image: 'totoro.jpg',
         title: 'Ratchet & Clank: Rift Apart',
         text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.'
     },
 
     {
-        image: 'img/03.webp',
+        image: 'la-citta-incantata.jpg',
         title: 'Fortnite',
         text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos."
     },
 
     {
-        image: 'img/04.webp',
+        image: '1198624-1920x1080-desktop-full-hd-studio-ghibli-wallpaper-photo.jpg',
         title: 'Stray',
         text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city'
     },
 
     {
-        image: 'img/05.webp',
+        image: 'Howl.jpg',
         title: "Marvel's Avengers",
         text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay."
     }
@@ -58,9 +58,8 @@ for (let i = 0; i < images.length; i++) {
 
     let imagesObject = images[i];
 
-    sliderElement.innerHTML += `<img src="${imagesObject.image}" >`;
-    thumbnailElement.innerHTML += `<div class= "cassone"><img src="${imagesObject.image}" ></div>`;
-
+    sliderElement.innerHTML += `<img src="./img/${imagesObject.image}" >`;
+    thumbnailElement.innerHTML += `<div class= "thumbnail-container"><img src="./img/${imagesObject.image}" ></div>`;
 }
 
 
